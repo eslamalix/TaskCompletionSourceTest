f980bbf82bba53331ceb30fc6ae79af4a45ef908
eslam ahmed ali hassan <eslam.pop911@yahoo.com>
2021-05-18 03:57:15 +0200

    more comments

--- TaskCompletionSourceTest/Program.cs
+++ TaskCompletionSourceTest/Program.cs
@@ -11,7 +11,7 @@ namespace TaskCompletionSourceTest
         /// TaskCompletionSource schedule work on threadpool introduce a task from other types maynot have async and await
         /// it does not start async operation its just a away to create awaitable content which potentially gets a result
         /// who ever await wont proceed continuation unless gets the result
-        /// 
+        /// use it to create awaitable code out of legacy code that does not leverage the task parralel library
         /// </summary>
         /// <param name="args"></param>
         /// <returns></returns>
@@ -21,7 +21,15 @@ namespace TaskCompletionSourceTest
             await WorkinNotebad();
             Console.WriteLine("Done");
         }
-        public static Task WorkinNotebad()
+        /// <summary>
+        /// public async static Task<object> WorkinNotebad()
+        /// return await source.Task;
+        /// if we introduced async and awiat to the following method is will inroduce another state machine that is not needed 
+        /// we always should let the caller who the main method above to take care of the async operation 
+        /// </summary>
+        /// <returns></returns>
+        
+        public  static Task<object> WorkinNotebad()
         {
           var source = new TaskCompletionSource<object>();
             var process = new Process
@@ -37,7 +45,8 @@ namespace TaskCompletionSourceTest
                 source.SetResult(null);
                 };
             process.Start();
-            return source.Task;
+            
+              return source.Task;
         }
     }
 }
